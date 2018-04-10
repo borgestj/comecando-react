fb219092139f86430395aaead43fcc169d375ff0
borgestj <borges.tj@gmail.com>
2018-04-09 21:44:49 -0300

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  mode: "production",
+  entry: './app/App.js',
+  output: {
+    filename: "public/bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react']
+        }
+      }
+    ]
+  }
+}
